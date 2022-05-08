1c3f6f3d8571ac8aa80d9abc31b4bf9d0515d479
Alexander Erkhov <93059454+Henus321@users.noreply.github.com>
2022-05-08 23:38:59 +0300

    added loading icon and media queries
    when a photo loads you see an animated icon and the button becomes inactive. Added media queries for mobile.

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const button = document.getElementById('btn');
 const dogContainer = document.getElementById('dog-container');
 const catContainer = document.getElementById('cat-container');
 const photo = document.getElementById('photo');
-let mode = 'cat';
+const loader = document.getElementById('loader');
+const loaderImg = document.getElementById('loader-img');
+const holderImg = document.getElementById('holder-image');
+let theme = 'cat';
 
 function switchTheme(event) {
   const checked = event.target.checked;
-  checked ? (mode = 'dog') : (mode = 'cat');
+  checked ? (theme = 'dog') : (theme = 'cat');
   checked
     ? document.documentElement.setAttribute('data-theme', 'dog')
     : document.documentElement.setAttribute('data-theme', 'cat');
@@ -16,21 +19,33 @@ function switchTheme(event) {
     : (button.textContent = 'Show me cats!');
   dogContainer.classList.toggle('transparent');
   catContainer.classList.toggle('transparent');
+  loaderImg.src = `loading-${theme}.svg`;
 }
 
 function addPhoto(data) {
+  photo.addEventListener('load', loaderToggle);
   photo.src = data;
+}
+
+function loaderToggle() {
+  loader.hidden = !loader.hidden;
+  buttonToggle();
+}
+
+function buttonToggle() {
+  button.disabled = !button.disabled;
 }
 
 async function getPhotos() {
   let apiUrl, link;
-  mode === 'cat'
+  loaderToggle();
+  theme === 'cat'
     ? (apiUrl = 'https://api.thecatapi.com/v1/images/search')
     : (apiUrl = 'https://dog.ceo/api/breeds/image/random');
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    mode === 'cat' ? (link = data[0].url) : (link = data.message);
+    theme === 'cat' ? (link = data[0].url) : (link = data.message);
     addPhoto(link);
   } catch (error) {
     console.log('Yay!', error);
