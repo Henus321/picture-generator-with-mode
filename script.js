d23007002923db1e1caf0d67ed1a3dad0ed3cf82
Alexander Erkhov <93059454+Henus321@users.noreply.github.com>
2022-05-08 18:02:10 +0300

    added APIs interaction with button and switcher
    button click now puts picture from one of two APIs, u can choose which one will put pictures by switcher.

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const button = document.getElementById('btn');
-
-// document.documentElement.setAttribute('data-theme', 'dog');
-// document.documentElement.setAttribute('data-theme', 'cat')
+const dogContainer = document.getElementById('dog-container');
+const catContainer = document.getElementById('cat-container');
+const photo = document.getElementById('photo');
+let mode = 'cat';
 
 function switchTheme(event) {
   const checked = event.target.checked;
+  checked ? (mode = 'dog') : (mode = 'cat');
   checked
     ? document.documentElement.setAttribute('data-theme', 'dog')
     : document.documentElement.setAttribute('data-theme', 'cat');
   checked
     ? (button.textContent = 'Show me dogs!')
     : (button.textContent = 'Show me cats!');
+  dogContainer.classList.toggle('transparent');
+  catContainer.classList.toggle('transparent');
 }
 
-function log() {
-  console.log(123);
+function addPhoto(data) {
+  photo.src = data;
+}
+
+async function getPhotos() {
+  let apiUrl, link;
+  mode === 'cat'
+    ? (apiUrl = 'https://api.thecatapi.com/v1/images/search')
+    : (apiUrl = 'https://dog.ceo/api/breeds/image/random');
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    mode === 'cat' ? (link = data[0].url) : (link = data.message);
+    addPhoto(link);
+  } catch (error) {
+    console.log('Yay!', error);
+  }
 }
 
 toggleSwitch.addEventListener('change', switchTheme);
-button.addEventListener('click', log);
+button.addEventListener('click', getPhotos);
